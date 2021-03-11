1ba285b6bc5862c9bfc525316be22e6c3cea30ee
Felipe Vieira <flpvieira86@gmail.com>
2021-03-11 16:04:47 -0300

    requisito 13 ok

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const net = require('net');
+const os = require('os');
 
 const { getLocationInfos } = require('./location');
 
 const getHeaderValue = (data, header) => {
-  const headerData = data
-    .split('\r\n')
-    .find((chunk) => chunk.startsWith(header));
+  const headerData = data.split('\r\n').find((chunk) => chunk.startsWith(header));
 
   return headerData.split(': ').pop();
 };
@@ -22,15 +21,24 @@ const server = net.createServer((socket) => {
     getLocationInfos(clientIP, (locationData) => {
       console.log(locationData);
       socket.write(startOfResponse);
-      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
+      socket.write(
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
+      );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
+      socket.write(
+        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>',
+      );
+      socket.write(`<p data-testid="arch">${os.platform()} - ${os.arch()} - ${os.release()}</p>`);
+      socket.write(`<p data-testid="cpu">${os.cpus()}</p>`);
+      socket.write(`<p data-testid="memory">${os.totalmem() / 1024 ** 3}</p>`);
       socket.write(`<p data-testid="device">${device}</p>`);
       socket.write(`<p data-testid="ip">${clientIP}</p>`);
       socket.write(`<p data-testid="city">${locationData.city}</p>`);
       socket.write(`<p data-testid="postal_code">${locationData.postal_code}</p>`);
-      socket.write(`<p data-testid="region">${locationData.region_name}, ${locationData.region}</p>`);
+      socket.write(
+        `<p data-testid="region">${locationData.region_name}, ${locationData.region}</p>`,
+      );
       socket.write(`<p data-testid="country">${locationData.country_name}</p>`);
       socket.write(`<p data-testid="company">${locationData.company}</p>`);
       socket.write('</body></html>');
