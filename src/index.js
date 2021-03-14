be6eaea84ea090223142db2ff1f4495e180de2cd
Rafael Assad <rafaassadeng@gmail.com>
2021-03-14 12:07:59 -0300

    lint

--- src/index.js
+++ src/index.js
@@ -26,14 +26,10 @@ const server = net.createServer((socket) => {
         company,
       } = locationData;
       socket.write(startOfResponse);
-      socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">'
-      );
+      socket.write('<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">');
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
-      socket.write(
-        '<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>'
-      );
+      socket.write('<iframe src="https://giphy.com/embed/l3q2zVr6cu95nF6O4" width="480" height="236" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>');
       socket.write(`<h2 data-testid="ip">${clientIP}</h2>`);
       socket.write(`<p data-testid="city">${city}</p>`);
       socket.write(`<p data-testid="postal_code">${postalCode}</p>`);
