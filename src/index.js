c2302320aed198252a28b8a89bd3b96463a520de
hleoc <informig@gmail.com>
2021-03-16 21:09:28 -0300

    Requisito 8

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ const endOfResponse = '\r\n\r\n';
 
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const clientIP = null;
+    const clientIP = getHeaderValue(data.toString(), 'X-Forwarded-For');
 
     getLocationInfos(clientIP, (locationData) => {
       socket.write(startOfResponse);
       socket.write(
-        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">'
+        '<html><head><meta http-equiv="content-type" content="text/html;charset=utf-8">',
       );
       socket.write('<title>Trybe 🚀</title></head><body>');
       socket.write('<H1>Explorando os Protocolos 🧐🔎</H1>');
